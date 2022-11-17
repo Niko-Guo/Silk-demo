f8b4f3c14abe34bea55f093deecc437321ca781d
GC <2845740149@qq.com>
2022-11-17 21:13:11 +0800

    refactor: contants and refact the authentification login

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,1 +1,7 @@
 export const EMPTY_STRING_PLACEHOLDER = '--';
+
+export const LOG_IN_URL =
+	'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCwKSCzYReCHtsBB42lVr9hSFKb6RZlbpY';
+
+export const SIGN_UP_URL =
+	'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCwKSCzYReCHtsBB42lVr9hSFKb6RZlbpY';
